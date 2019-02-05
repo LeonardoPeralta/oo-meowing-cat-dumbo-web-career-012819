e6dac2df1dc7bf1134ff53e186c25169dabd3308
Leonardo Peralta <leo.pm15@gmail.com>
2019-02-05 03:42:30 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,4 @@
-## code your solution here. 
+## code your solution here.
 class Cat
 
 attr_accessor :meow
@@ -6,5 +6,5 @@ attr_accessor :meow
 def initialize(meow)
     @meow = meow
   end
-  
+
 end
